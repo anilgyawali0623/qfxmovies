fd6e1060f1f912f762af667935946eda6aaee58b
anilgyawali0623 <anilgyawali8937@gmail.com>
2025-09-05 16:49:45 +0545

    created utils for error handling

---
+++ backend/utils/apiResponse.js
@@ -0,0 +1,11 @@
+//  this is the util file which is created to
+class ApiResponse {
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export default { ApiResponse };
